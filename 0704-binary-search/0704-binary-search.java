e0c7709ca3a21d0041537b9e9250fd2dba954fd1
reena200501 <reenar.cse2023@dce.edu.in>
2026-07-02 15:12:15 +0530

    Time: 0 ms (100%), Space: 47.7 MB (97.53%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] A, int target) {
+        int n=A.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target==A[mid]) return mid;
+            else if(target>A[mid]) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+}
